1f0c5c226bb37a7a6730285b80e65ef7c87a4394
Alan Filipe Cardozo Fabeni <alanfcf@gmail.com>
2019-06-09 12:42:08 -0300

    tests

---
+++ src/math.js
@@ -0,0 +1,31 @@
+const calcTip = (total, tipPercent = 0.2) => {
+    const tip = total * tipPercent
+
+    return total + tip
+}
+
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Error')                
+            }
+            resolve(a + b)
+        })
+    })
+}
+
+module.exports = {
+    calcTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit,
+    add
+}
---
+++ tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,8 @@
+module.exports = {
+    setApiKey() {
+        console.log('setApiKey mock')
+    },
+    send() {
+        console.log('send mock')
+    }
+}
